b71c104b94348ef795539de482482dd4ccca889f
Devuzz09 <devikalakshmi09@gmail.com>
2025-08-02 12:20:11 +0530

    Third update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
+#create templates folder ==> index.html
 def index():
-    return "Welcome to MRECW"
+    return render_template("index.html")
 @app.route("/home")
 def home():
     return "Home page"
